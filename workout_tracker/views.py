8e2e737b849306446ddb1e5ad1a9dee1c980102a
allyporwal <allyporwal@gmail.com>
2021-03-09 20:32:28 +0000

    Add to workout_tracker views to test data input

--- workout_tracker/views.py
+++ workout_tracker/views.py
@@ -4,8 +4,15 @@ from .forms import WorkoutTrackerForm
 
 # Create your views here.
 def track_workout(request):
+
     if request.method == 'POST':
-        form = WorkoutTrackerForm()
+        form_data = {
+            'session_name': request.POST['session_name'],
+            'workout': request.POST['workout'],
+        }
+        form = WorkoutTrackerForm(form_data)
+        if form.is_valid:
+            form.save()
 
     form = WorkoutTrackerForm()
     template = 'workout_tracker/track_workout.html'
