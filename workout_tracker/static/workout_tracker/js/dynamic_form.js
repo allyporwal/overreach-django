40541cd829ae6d6058f732fab28cc66095531f77
allyporwal <allyporwal@gmail.com>
2021-03-30 20:24:39 +0000

    Add to and refine dynamic active workout form

--- workout_tracker/static/workout_tracker/js/dynamic_form.js
+++ workout_tracker/static/workout_tracker/js/dynamic_form.js
@@ -26,35 +26,52 @@
 //         placeholder="reps"><br>`)
 // });
 
-$('.log-exercise').click(function(e) {
-    let workoutSets = $("#workout-sets");
-    let numberOfSets = $('.set-count').last().val()
-    console.log(numberOfSets)    
-    let exerciseNumber = $('.set-count').last().attr("id")
-    let x = parseInt(exerciseNumber.slice(9, 10))
+$('#workout-sets').on('click', '.log-exercise', function() {
+    let numberOfSets = $('.set-count').last().val();
+    let exerciseNumber = $('.set-count').last().attr("id");
+    let x = parseInt(exerciseNumber.slice(9, 10));
     for (i = 0; i < numberOfSets; i++) {
-        $('#save').before(
-            `<div class="row input-group d-inline-flex justify-content-center">
-                    <div class="col-1"></div>
-                    <input type="number" step="0.5" name="weight-${i+1}-exercise-${x}" placeholder="weight" class="col-4">
-                    <input type="number" name="reps-${i+1}-exercise-${x}" placeholder="reps" class="col-2">
-                    <input type="number" step="0.5" name="rpe-${i+1}-exercise-${x}" placeholder="rpe" class="col-2">
-                    <div class="input-group-append col-2 p-0">
-                        <button class="btn btn-outline-secondary w-100" type="button" id="delete-set-${i+1}">X</button>
-                    </div>
-                    <div class="col-1"></div>
+        $('#button-container').before(
+            `<div class="form-row">
+                <div class="form-group col-6">
+                    <label class="sr-only" for="weight-${i+1}-exercise-${x}">Weight</label>
+                    <input type="number" step="0.5" name="weight-${i+1}-exercise-${x}" placeholder="Weight lifted" class="form-control">
                 </div>
-                <br>`);
+                <div class="form-group col-2">
+                    <label class="sr-only" for="reps-${i+1}-exercise-${x}">Reps</label>
+                    <input type="number" name="reps-${i+1}-exercise-${x}" placeholder="Reps" class="form-control">
+                </div>
+                <div class="form-group col-2">
+                    <label class="sr-only" for="rpe-${i+1}-exercise-${x}">RPE</label>
+                    <input type="number" step="0.5" name="rpe-${i+1}-exercise-${x}" placeholder="RPE" class="form-control">
+                </div>
+                <div class="form-group col-2">
+                    <label class="sr-only" for="delete-set-${x}">Delete set</label>
+                    <button class="btn btn-outline-secondary w-100 form-control" name="delete-set-${x}" type="button"><i class="fas fa-trash"></i></button>                
+                </div>    
+            </div>`);
 }});
 
-$('#add-exercise').click(function(e) {
+$('#add-exercise').click(function() {
     let workoutSets = $("#workout-sets");
-    let exerciseNumber = workoutSets.children('.exercise-name').last().attr("id")
-    let x = parseInt(exerciseNumber.slice(9, 10)) + 1
-    $('#save').before(`<input type="text" id="exercise-${x}-name" 
-        class="exercise-name" name="exercise-${x}-name" placeholder="exercise_name">
-        <input id="exercise-${x}-sets" class="set-count" type="number" 
-        name="exercise-${x}-sets" placeholder="sets">
-        <input id="exercise-${x}-sets" type="number" name="exercise-${x}-reps" 
-        placeholder="reps"><br>`)
+    let exerciseNumber = $('.exercise-name').last().attr("id");
+    let x = parseInt(exerciseNumber.slice(9, 10)) + 1;
+    $('#button-container').before(`<div class="form-row exercise-row">
+                          <div class="form-group col-6">
+                              <label for="exercise-${x}-name">Exercise ${x}</label>
+                              <input type="text" id="exercise-${x}-name" class="exercise-name form-control" name="exercise-${x}-name">
+                          </div>
+                          <div class="form-group col-2">
+                              <label for="exercise-${x}-sets">Sets</label>
+                              <input id="exercise-${x}-sets" class="set-count form-control" type="number" name="exercise-${x}-sets">
+                          </div>
+                          <div class="form-group col-2">
+                              <label for="exercise-${x}-reps">Reps</label>
+                              <input id="exercise-${x}-reps" class="form-control" type="number" name="exercise-${x}-reps">
+                          </div>
+                          <div class="form-group col-2">
+                              <label for="log-exercise-${x}">&nbsp;</label>
+                              <button id="log-${x}" class="btn btn-outline-secondary w-100 form-control log-exercise" name="log-exercise-${x}" type="button">Log</button>                
+                          </div>
+                      </div>`)
 });
