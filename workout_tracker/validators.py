9008811d7562340956b80ab50fe89af870b60f6e
allyporwal <allyporwal@gmail.com>
2021-04-12 13:28:53 +0000

    Fix small issues in dashboard and profiles templates

--- workout_tracker/validators.py
+++ workout_tracker/validators.py
@@ -3,8 +3,8 @@ def validate_active_workout(workout):
     before saving to to Django JSONField"""
     # Add all workout values to arrays to be checked
     exercise_names = [exercise['exercise'] for exercise in workout]
-    sets = [exercise['sets'] for exercise in workout]
-    reps = [exercise['reps'] for exercise in workout]
+    sets = [int(exercise['sets']) for exercise in workout]
+    reps = [int(exercise['reps']) for exercise in workout]
     weights = []
     reps_lifted = []
     rate_perceived_exertion = []
@@ -13,43 +13,43 @@ def validate_active_workout(workout):
 
     for exercise in workout:
         for weight in exercise['set_volumes']:
-            weights.append(weight['weight'])
-            reps_lifted.append(weight['rep_count'])
-            rate_perceived_exertion.append(weight['rpe'])
+            weights.append(float(weight['weight']))
+            reps_lifted.append(int(weight['rep_count']))
+            rate_perceived_exertion.append(float(weight['rpe']))
 
     if '' in exercise_names:
         errors.append("Please ensure you input a name for every exercise")
 
     for set_number in sets:
-        if int(set_number) < 0:
-            errors.append("You can't lift negative sets!")
+        if set_number <= 0:
+            errors.append("You can't lift negative or zero sets!")
         elif set_number == '':
             errors.append("Please input how many sets you want to lift")
 
     for rep in reps:
-        if int(rep) < 0:
+        if rep < 0:
             errors.append("You can't lift negative reps!")
-        if int(rep) > 250:
+        if rep > 250:
             errors.append("You can't log more than 250 reps of one exercise \
                           in a single set")
 
     for weight in weights:
-        if float(weight) < 0:
+        if weight < 0:
             errors.append("You can't lift negative kilos!")
-        if float(weight) > 1200:
+        if weight > 1200:
             errors.append("You can't lift more than 1200kg in a single rep")
 
     for reps_logged in reps_lifted:
-        if int(reps_logged) < 0:
+        if reps_logged < 0:
             errors.append("You can't lift negative reps!")
-        if int(reps_logged) > 250:
+        if reps_logged > 250:
             errors.append("You can't log more than 250 reps of one exercise \
                           in a single set")
 
     for rpe in rate_perceived_exertion:
-        if float(rpe) < 1:
+        if rpe < 1:
             errors.append("RPE must be between 1 and 10")
-        if float(rpe) > 10:
+        if rpe > 10:
             errors.append("RPE must be between 1 and 10")
 
     return errors
