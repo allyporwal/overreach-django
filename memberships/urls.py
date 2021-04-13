5520cc54bdfb6f93785ef1f0d385693c7b826e22
allyporwal <allyporwal@gmail.com>
2021-04-13 15:41:08 +0000

    Add templates and start of views for subscription management

--- memberships/urls.py
+++ memberships/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.memberships, name='memberships'),
+    path('signup/', views.membership_signup, name='membership_signup'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
--- memberships/views.py
+++ memberships/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+from django.conf import settings
 
 
 # Create your views here.
-def memberships(request):
-    return render(request, 'memberships/memberships.html')
+def membership_signup(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('checkout'))
+
+    form = UserProfileForm(instance=profile)
+    template = 'memberships/membership_signup.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
+
+
+def checkout(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    template = 'memberships/checkout.html'
+    context = {
+        'profile': profile,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'test',
+    }
+    return render(request, template, context)
