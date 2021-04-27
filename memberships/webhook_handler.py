1c68991a0b639e664071eb1b4caf71478f8f066b
allyporwal <allyporwal@gmail.com>
2021-04-27 12:33:13 +0000

    Remove some code to test webhook handler

--- memberships/webhook_handler.py
+++ memberships/webhook_handler.py
@@ -13,23 +13,23 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    def _send_welcome_email(self, email_data):
-        """send the user a welcome email when their subscription
-        has been created"""
-        user_email = email_data.email
-        subject = render_to_string(
-            'memberships/welcome_emails/welcome_email_subject.txt',
-            {'email_data': email_data})
-        body = render_to_string(
-            'memberships/welcome_emails/welcome_email_body.txt',
-            {'email_data': email_data,
-             'contact_email': settings.DEFAULT_FROM_EMAIL})
+    # def _send_welcome_email(self, email_data):
+    #     """send the user a welcome email when their subscription
+    #     has been created"""
+    #     user_email = email_data.email
+    #     subject = render_to_string(
+    #         'memberships/welcome_emails/welcome_email_subject.txt',
+    #         {'email_data': email_data})
+    #     body = render_to_string(
+    #         'memberships/welcome_emails/welcome_email_body.txt',
+    #         {'email_data': email_data,
+    #          'contact_email': settings.DEFAULT_FROM_EMAIL})
 
-        send_mail(subject,
-                  body,
-                  settings.DEFAULT_FROM_EMAIL,
-                  [user_email]
-                  )
+    #     send_mail(subject,
+    #               body,
+    #               settings.DEFAULT_FROM_EMAIL,
+    #               [user_email]
+    #               )
 
     def handle_event(self, event):
         """Handle a generic/unknown/unexpected webhook event"""
@@ -48,15 +48,6 @@ class StripeWH_Handler:
                 stripe_customer_id=stripe_customer_id)
             profile.stripe_subscription_id = subscription_id
             profile.save()
-            # data for email send
-            first_name = profile.first_name
-            email = profile.default_billing_email
-            email_data = {
-                'first_name': first_name,
-                'email': email,
-                'subscription_id': subscription_id,
-            }
-            self._send_welcome_email(email_data)
 
             return HttpResponse(
                 content=f'Webhook received: {event["type"]}.\
@@ -75,13 +66,7 @@ class StripeWH_Handler:
         try:
             profile = UserProfile.objects.get(
                 stripe_customer_id=stripe_customer_id)
-            # first_name = profile.first_name
-            # email = profile.default_billing_email
-            # email_data = {
-            #     'first_name': first_name,
-            #     'email': email,
-            #     'subscription_id': subscription_id,
-            # }
+
             profile.is_subscribed = False
             profile.stripe_subscription_id = f'Cancelled {subscription_id}'
             profile.save()
